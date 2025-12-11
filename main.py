6da079b0029f7544f7fdbcd21c9174045464036c
Marta Domagała <mahomegol@gmail.com>
2025-12-11 12:01:44 +0100

    Add ByteTrack to track footballers in tracker.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import cv2
 import argparse
+import cv2
+import supervision as sv
 from src.inference import detect_objects
+from src.tracker import create_tracker
 
-#Configuration for arguemnts parser
+# Configuration for arguments parser
 parser = argparse.ArgumentParser("System analizy piłkarskiej")
 parser.add_argument('--source_video_path', type=str, required=True, help='source video path')
 args = parser.parse_args()
@@ -13,10 +15,17 @@ cap = cv2.VideoCapture(path)
 
 nr_of_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
+tracker = create_tracker()
+
+bounding_box_annotator = sv.BoundingBoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
 for i in range(nr_of_frames):
     ret, frame = cap.read()
     if ret:
         results = detect_objects(frame)
+        detections = sv.Detections.from_ultralytics(results)
+        detections = tracker.update_with_detections(detections)
         annotated_frame = results.plot()
         cv2.imshow("frame", annotated_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
---
+++ src/tracker.py
@@ -0,0 +1,5 @@
+from supervision import ByteTrack
+
+def create_tracker():
+    byte_track = ByteTrack()
+    return byte_track
